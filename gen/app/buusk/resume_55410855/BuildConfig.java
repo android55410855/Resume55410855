97f4ad351031f18a39732484c52b792f4d605ee3
android55410855 <sirilakwailom@gmail.com>
2014-09-03 16:54:23 +0700

    waeo

---
+++ gen/app/buusk/resume_55410855/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package app.buusk.resume_55410855;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/app/buusk/resume_55410855/imageActivity.java
@@ -0,0 +1,13 @@
+package app.buusk.resume_55410855;
+
+//import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class imageActivity extends Activity{
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.image_sirilak);
+	}
+}
+
